344aabdb57b4c563ca329c846fe536c5059b1adb
bly <bly930725@gmail.com>
2016-10-20 13:31:16 -0600

    add config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+
+# ------------------ PATH ------------------
+
+# ------------------ PARAM -----------------
+
+MEMORY_CAPACITY = 10000
+OBSERVE = 500
+BATCH_SIZE = 500
+GAMMA = 0.99
+EPSILON_DECAY = 0.95
+EPSILON_DECAY_TIME = 100
+
+# ------------------ OTHER -----------------
+
+RANDOM_SEED = 2016
+RENDER = False
